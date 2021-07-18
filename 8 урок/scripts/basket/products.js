a51f07912fa5508983e9541137ee40668c0648b5
Evgeniy <Dgef2008@yandex.ru>
2021-07-18 12:40:12 +0300

    Lesson_8

---
+++ 8 урок/scripts/basket/products.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const products = [
+    new ProductDTO(
+        0,
+        'catalog_1.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        1,
+        'catalog_2.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        2,
+        'catalog_3.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        3,
+        'catalog_4.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        4,
+        'catalog_5.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        5,
+        'catalog_6.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        6,
+        'catalog_7.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+    new ProductDTO(
+        7,
+        'catalog_8.png',
+        'Mango People T-shirt',
+        52.00,
+    ),
+];
